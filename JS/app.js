5efb412b3c5a0dd48d8723e484bcbd394562122f
laupadron <laupadron1458@gmail.com>
2022-10-13 19:07:33 -0300

    ecommerce

--- JS/app.js
+++ JS/app.js
@@ -2,67 +2,41 @@ const shopItems = document.querySelector('.shop-items');
 const buttonAdd = document.querySelectorAll(' .btn-one');
 const cartsItems =document.querySelectorAll('.section');
 const allItemShop = document.querySelectorAll('.shop-items')
-
+//arreglo de items del carrito
 let arrayItems =[]; 
+//local storage para que persista
+//¿¿¡¡¡¡ PROBLEMAS CON LAS IMAGENES!!!
 arrayItems = JSON.parse(localStorage.getItem("items"));
-addToCart()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//evento click de los botones del carrito
 buttonAdd.forEach(addItemShop =>{
  addItemShop .addEventListener('click', addToCart );
  console.log();
 });
 
+//funcion para agregar item con click
 function addToCart(event){
  const boton =event.target;
- 
+ //variable donde guardo la seccion mas cercana al click
  const itemComplete = boton.closest ('.section');
- 
+ //captura de elementos de los items
  const itemTitle= itemComplete.querySelector('.h3').textContent;
- 
  const itemPrice =itemComplete.querySelector('.price').textContent;
  const itemImage = itemComplete.querySelector('.item-img').src
  
- 
- 
- addItemToCart(itemTitle, itemPrice,itemImage);
- 
- 
- 
+ // llamada de funcion de agregar al carrito
+addItemToCart(itemTitle, itemPrice,itemImage);
 }
 
 
-
+//funcion para agregaral carrito
 function addItemToCart(itemTitle, itemPrice,itemImage){
  
- 
- 
-
- 
- 
-
-/*for(let i=0;i<arrayItems.length;i++){
- if(arrayItems[i].=== itemTitle){
-  console.log(byTitle[i])
- }
-}*/
-
+ //local storage para captura de datos
 localStorage.setItem("items", JSON.stringify(arrayItems))
 
+//crear elemento div
  const itemCartRow = document.createElement('div');
  
  const cartContent = 
@@ -76,14 +50,17 @@ localStorage.setItem("items", JSON.stringify(arrayItems))
    </form>
    <button class="aside-btn">X</button>
   </div>`;
- 
-  arrayItems.push({itemTitle,itemPrice });
+ //push de items al array 
+ arrayItems.push({itemTitle,itemPrice });
   
+ //pintar en html
  itemCartRow.innerHTML = cartContent;
  shopItems.append(itemCartRow);
  
+ //click de boton para delete
  itemCartRow.querySelector('.aside-btn').addEventListener('click', deleteItem);
-  totalCart();
+ //llamada de funcion de suma de items
+ totalCart();
 }
 
 
@@ -95,21 +72,25 @@ function totalCart(){
  
  itemsShop.forEach((element)=>{
   const cartPrice = element.querySelector('.price-item');
+  //convierto a numero para sumar
   const itemPrice = Number(cartPrice.textContent.replace('$',''));
   
   const cantItems = element.querySelector('.input');
   
   const itemCant = Number(cantItems.value);
   
-
+  //¡¿¿¿por que no me multiplica?? ver luego
   total+= itemPrice*itemCant;
  
  });
+ //pintar total 
  totalCart.innerHTML= `$ ${total}`
 }
 
+//función para delete item
 function deleteItem(element){
  const clicked = element.target
  clicked.closest('.item').remove();
+ //¡¿¿como pongo en 000 el total??
  totalCart();
 }
